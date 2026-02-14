013076453090aee0260f529689f9fe018663ecf5
Moufid Zekiouk <moufid.zekiouk@studenti.unina.it>
2026-02-14 22:18:50 +0000

    procedure aggiunto

--- lettori-scrittori_su_piu_oggetti_monitor/procedure.c
+++ lettori-scrittori_su_piu_oggetti_monitor/procedure.c
@@ -7,11 +7,22 @@ void inizializza(struct monitor* m){
 	m->id_treno=0;
 
 	/* TBD: Inizializzare le variabili dell'algoritmo, il mutex, e le variabili condition */
+	m->num_scrittori = 0;
+	m->num_cv_scrittori = 0;
+	m->num_lettori = 0;
+	m->num_cv_lettori = 0;
+
+	pthread_mutex_init(&m->mutex, NULL);
+	pthread_cond_init(&m->cv_scrittori, NULL);
+	pthread_cond_init(&m->cv_lettori, NULL);
 }
 
 void rimuovi (struct monitor* m){
 
 	/* TBD: Disattivare mutex e variabili condition */
+	pthread_mutex_destroy(&m->mutex);
+	pthread_cond_destroy(&m->cv_scrittori);
+	pthread_cond_destroy(&m->cv_lettori);
 }
 
 
@@ -27,12 +38,32 @@ void scrivi_stazione(struct monitor* m, int stazione){
 	 * 		La variabile "num_cv_scrittori" deve essere incrementata subito prima
 	 * 		di wait_cond(), e decrementata subito dopo di wait_cond().
 	 */
+	pthread_mutex_lock(&m->mutex);
+	
+	while (m->num_lettori >0 || m->num_scrittori >0){
+	m->num_cv_scrittori++;
 
+	pthread_cond_wait(&m->cv_scrittori, &m->mutex);
+
+	m->num_cv_scrittori--;
+}	
+	m->num_scrittori=1;
+	pthread_mutex_unlock(&m->mutex);
 
 	// SCRITTURA
 	m->stazione=stazione;
 	printf("Scrittura: stazione=%d\n", stazione);
 
+	pthread_mutex_lock(&m->mutex);
+	
+	m->num_scrittori=0;
+
+	if(m->num_cv_scrittori>0){
+	pthread_cond_signal(&m->cv_scrittori);	
+}	else{
+	pthread_cond_broadcast(&m->cv_lettori);
+}
+	pthread_mutex_unlock(&m->mutex);
 
 }
 
@@ -50,13 +81,32 @@ int leggi_stazione(struct monitor* m){
 	 * 		di wait_cond(), e decrementata subito dopo di wait_cond().
 	 */
 
+	pthread_mutex_lock(&m->mutex);
 
+	while(m->num_scrittori>0){
+	m->num_cv_lettori++;
+
+	pthread_cond_wait(&m->cv_lettori, &m->mutex);
+
+	m->num_cv_lettori--;
+}
+	m->num_lettori++;
+
+	pthread_mutex_unlock(&m->mutex);
 
 	// LETTURA
 	int ris=m->stazione;
 	printf("Lettura: stazione=%d\n", ris);
 
+	pthread_mutex_lock(&m->mutex);
 
+	m->num_lettori--;
+
+	if(m->num_lettori==0  && m->num_cv_scrittori>0){
+	pthread_cond_signal(&m->cv_scrittori);
+}	
+
+	pthread_mutex_unlock(&m->mutex);
 	return ris;
 }
 
